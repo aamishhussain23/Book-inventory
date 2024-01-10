0ca505634f290d30fe4beae1f24caff16e1feea9
Aamish Hussain <aamishhussain23@gmail.com>
2024-01-10 23:20:22 +0530

    added database connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./app')
-
+const connectDB = require('./data/database')
 
 app.listen(process.env.PORT, () => {
+    connectDB()
     console.log(`server is running on http://localhost:${process.env.PORT}`)
 })
